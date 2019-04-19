35db677d382de890b6a453c726654d0fb53a41bc
Thomas Pucci <thomas.lb.pucci@gmail.com>
2019-04-19 13:45:47 +0200

    :wrench: (example) Config test runner in example project

--- example/jest.config.js
+++ example/jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  moduleDirectories: ['node_modules', '<rootDir>/../node_modules'],
+  moduleDirectories: ['node_modules', 'example/node_modules'],
+  rootDir: '..',
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/src/'],
 };
