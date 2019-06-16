f4f55958fa4895c93790d816fc99f6a2903fc042
Thomas Pucci <thomas.lb.pucci@gmail.com>
2019-06-16 18:45:18 +0200

    :construction: (BackHandlerDelegate) observes back presses

--- src/Navigation/BackHandlerDelegate.ts
+++ src/Navigation/BackHandlerDelegate.ts
@@ -1,12 +1,32 @@
 import { BackHandler } from 'react-native';
+import { fromEventPattern } from 'rxjs';
+import { map, elementAt } from 'rxjs/operators';
 
 export class BackHandlerDelegate {
+  private shouldExit: boolean = false;
   constructor() {
-    BackHandler.addEventListener('hardwareBackPress', this.backHandler);
+    const addHandler = (handler: () => void) => {
+      BackHandler.addEventListener('hardwareBackPress', () => {
+        handler();
+        return !this.shouldExit;
+      });
+    };
+    const hardwareBacks$ = fromEventPattern<undefined>(addHandler);
+    hardwareBacks$
+      .pipe(map(this.resetShouldExit))
+      .pipe(
+        map(() => {
+          // tslint:disable-next-line
+          console.log("back");
+        })
+      )
+      .pipe(elementAt(3))
+      .subscribe(this.setShouldExit);
   }
-
-  private backHandler = () => {
-    // tslint:disable-next-line
-    console.log("Back action fired !");
+  private resetShouldExit = () => {
+    this.shouldExit = false;
+  };
+  private setShouldExit = () => {
+    this.shouldExit = true;
   };
 }
