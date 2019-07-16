ca5a3ed62d97e6d73de662d110c0f76dc0eb13ce
Thomas Pucci <thomas.lb.pucci@gmail.com>
2019-07-16 13:58:22 +0200

    :construction: (example/App) add menu to choose example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
 import { SignIn } from './canals/SignIn';
 import { FullScreenPortal } from 'react-gondola';
+import { View, TouchableOpacity, Text } from 'react-native';
 
-export default class App extends Component<{}> {
+interface State {
+  example: null | 'SignIn' | 'Instagram';
+}
+
+const EXAMPLES = { SignIn, Instagram: SignIn };
+
+export default class App extends Component<{}, State> {
+  state: State = {
+    example: null,
+  };
+
   render() {
+    if (!this.state.example) {
+      return (
+        <View style={{ alignItems: 'center', flex: 1, justifyContent: 'center' }}>
+          {Object.keys(EXAMPLES).map(example => (
+            <TouchableOpacity
+              key={example}
+              onPress={() => {
+                // @ts-ignore
+                this.setState({ example });
+              }}
+              style={{
+                paddingHorizontal: 16,
+                paddingVertical: 8,
+                backgroundColor: 'cyan',
+                borderRadius: 4,
+                marginTop: 8,
+              }}
+            >
+              <Text>{example}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      );
+    }
+
+    const Example = EXAMPLES[this.state.example];
+
     return (
       <FullScreenPortal>
-        <SignIn />
+        <Example />
       </FullScreenPortal>
     );
   }
