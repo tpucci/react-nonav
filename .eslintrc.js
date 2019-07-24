fc7436094b3599c14f93b0ffe0c36543867afc70
Thomas Pucci <thomas.lb.pucci@gmail.com>
2019-07-24 20:02:44 +0200

    :art: (eslintrc) disable warning for explicit any

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     '@typescript-eslint/indent': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
   settings: {
     react: {
