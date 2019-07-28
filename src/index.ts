df1c5ed3b29fe299897f334f3a2bbb7e2a8389bf
Thomas Pucci <thomas.lb.pucci@gmail.com>
2019-07-28 13:10:34 +0200

    :bug: (recompose) fix error due to import order
    This is a known issue of compatibility between recompose and rxjs. See
    https://stackoverflow.com/questions/53878650/you-provided-an-invalid-object-where-a-stream-was-expected-when-using-rxjs6-an.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+/**
+ * Import order is important between recompose and rxjs.
+ * We need to import recompose first in order for it to polyfill observable symbol.
+ * @see https://stackoverflow.com/questions/53878650/you-provided-an-invalid-object-where-a-stream-was-expected-when-using-rxjs6-an.
+ */
+import 'recompose';
+
 export { FullScreenPortal } from './FullScreenPortal';
 export { Screen } from './Screen';
 export { Canal } from './Canal';
+export { Navigation } from './Navigation';
